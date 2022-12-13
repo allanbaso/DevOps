93b506f89369bcc7e16cd29528b75e26ea511207
Allan B <allanbs.88@gmail.com>
2022-12-13 15:12:15 -0600

    Revisando las conx a mysql

--- learn-python3-master/samples/db/do_mysql.py
+++ learn-python3-master/samples/db/do_mysql.py
@@ -9,23 +9,19 @@
 import mysql.connector
 
 # change root password to yours:
-conn = mysql.connector.connect(user='root', password='password', database='test')
+conn = mysql.connector.connect(user='allan@localhost:3306', password='allan1', database='test_db')
 
 cursor = conn.cursor()
-# 创建user表:
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
-# 插入一行记录，注意MySQL的占位符是%s:
-cursor.execute('insert into user (id, name) values (%s, %s)', ('1', 'Michael'))
+#cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into test_table (firstName, lastName) values (%s, %s)', ('Allan', 'Barrientos'))
 print('rowcount =', cursor.rowcount)
-# 提交事务:
 conn.commit()
 cursor.close()
 
 # 运行查询:
 cursor = conn.cursor()
-cursor.execute('select * from user where id = %s', ('1',))
+cursor.execute('select * from test_table')
 values = cursor.fetchall()
 print(values)
-# 关闭Cursor和Connection:
 cursor.close()
 conn.close()
