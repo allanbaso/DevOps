088aa473fb693f9a5cf03366aa02e6c9964aae1e
Allan B <allanbs.88@gmail.com>
2022-12-15 12:58:30 -0600

    leerXls.py lee toda la lista del xls y valida por systema y stage

--- PythonXlsReader-master/leerXls.py
+++ PythonXlsReader-master/leerXls.py
@@ -1,14 +1,19 @@
 import xlrd
 
-# Open the Workbook
-workbook = xlrd.open_workbook("Sample_moves.xls")
-
-# Open the worksheet
-worksheet = workbook.sheet_by_index(0)
-
-# Iterate the rows and columns
-for i in range(0, 16):
-    for j in range(0, 1):
-        # Print the cell values with tab space
-        print(worksheet.cell_value(i, j), end='\t')
-    print('')
+book = xlrd.open_workbook("Book4.xls")
+sh = book.sheet_by_index(0)
+for col in range(sh.ncols):
+    for rx in range(sh.nrows):
+        fila = sh.row_values(rx)
+        new_list = [word.strip() for word in fila]#se le quita los espacios y caracteres demas
+if "HOLDPROD" in new_list:
+    #print("El nombre del elmento #"+str(rx+1)+" de la lista es : "+str(new_list))
+    if "H" in new_list:
+        print('Listas para borrar Libman2 con stage 2')
+    else:
+        print('Lista para borrar en Libman2')
+if "PROD" in new_list:
+    if "H" in new_list:
+        print('No existe stage H en Libman1')
+    else:
+        print('Lista para borrar en Libman1')
